8d8eaa638bb6b04988e9bd90aaa9312497e1be9d
brooklyn-rgb <brooklynstore151@gmail.com>
2026-01-17 22:17:22 -0800

    updates

---
+++ Pages/AnnouncementBar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace macfish.Pages
+{
+    public class AnnouncementBarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
